c469a6efedf0ca938c8fd0dfeeedd4df31284daf
AdarshBelnekar <adarshbelnekar123@gmail.com>
2025-02-05 10:43:49 +0530

    first commit

--- src/components/Dashboard/Sign.jsx
+++ src/components/Dashboard/Sign.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from 'react'
-import { useNavigate } from 'react-router-dom'
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Sign = () => {
-    const navigate = useNavigate()
-    const [email, setEmail] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
 
   const handleLogin = () => {
@@ -15,37 +15,46 @@ const Sign = () => {
       // Show alert if credentials are invalid
       alert('Invalid email or password');
     }
-}
-
+  };
 
   return (
-    <div>
-         <div className="flex justify-center items-center h-screen bg-gray-100">
-      <div className="bg-white p-6 rounded-lg shadow-lg w-96">
-      <h2 className="text-center text-2xl font-bold mb-4">Sign in</h2>
+    <div className="h-screen w-full flex justify-center items-center bg-gray-100">
+      <div className="bg-white p-6 rounded-lg shadow-lg w-96 flex flex-col items-center">
+        
+        {/* Logo */}
+        <img src="./logo.jpg" className="h-16 w-16 mb-6" alt="Logo" />
+        
+        <h2 className="text-center text-2xl font-bold mb-4">Sign in</h2>
+        
         <label>Your Email</label>
         <input 
-        value={email}
-        name='email'
-        onChange={(e)=>{
-            setEmail(e.target.value)
-        }}
-        type="email" placeholder="Email@example.com" className="border p-2 w-full rounded mb-2" />
+          value={email}
+          name="email"
+          onChange={(e) => setEmail(e.target.value)}
+          type="email"
+          placeholder="Email@example.com"
+          className="border p-2 w-full rounded mb-2"
+        />
         
         <label>Password</label>
-       
         <input 
-        value={password}
-        name='password'
-        onChange={(e) => setPassword(e.target.value)}
+          value={password}
+          name="password"
+          onChange={(e) => setPassword(e.target.value)}
+          type="password"
+          placeholder="Password"
+          className="border p-2 w-full rounded mb-4"
+        />
         
-        type="password" placeholder="Password" className="border p-2 w-full rounded mb-4" />
-        
-        <button onClick={handleLogin}className="bg-green-500 w-full text-white p-2 rounded">Sign in</button>
+        <button
+          onClick={handleLogin}
+          className="bg-green-500 w-full text-white p-2 rounded"
+        >
+          Sign in
+        </button>
       </div>
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default Sign
+export default Sign;
